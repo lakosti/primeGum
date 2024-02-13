b5a1737756510c969df0dcf996d020a0181015fe
lakosti <101802568+lakosti@users.noreply.github.com>
2024-02-13 13:51:38 +0000

    Deploying to gh-pages from @ lakosti/primeGum@1bcd4bbb297ce12b5efe7e07622fa4e1fc5740ba 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-33d25b6e.js";const s=()=>{const n=document.querySelector(".js-close-menu"),o=document.querySelector(".js-open-menu"),e=document.querySelector(".js-menu-container");n.addEventListener("click",t),o.addEventListener("click",c);function t(){e.classList.add("is-hidden")}function c(){e.classList.remove("is-hidden")}};s();
+//# sourceMappingURL=commonHelpers.js.map
